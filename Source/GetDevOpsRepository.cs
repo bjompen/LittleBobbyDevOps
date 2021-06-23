c248c5c150e258bcd1694dcda70aa9205f83cc20
Björn Sundling <bjorn.sundling@advania.com>
2021-06-23 22:31:14 +0200

    MyFirstCmdLet©

--- Source/GetDevOpsRepository.cs
+++ Source/GetDevOpsRepository.cs
@@ -57,4 +57,46 @@ namespace PipeHow.LittleBobbyDevOps
             }
         }
     }
+
+    [Cmdlet(VerbsCommon.Get, "DevOpsRepositoryBranches")]
+    public class GetDevOpsRepositoryBranches : PSCmdlet
+    {
+        /// <summary>
+        /// <para type="description">The collection uri to the DevOps organisation.</para>
+        /// </summary>
+        [Parameter(Mandatory = true)]
+        [ValidateNotNullOrEmpty()]
+        public string CollectionUri { get; set; }
+
+        /// <summary>
+        /// <para type="description">The Personal Access Token used for authentication.</para>
+        /// </summary>
+        [Parameter(Mandatory = true)]
+        [ValidateNotNullOrEmpty()]
+        public string PAT { get; set; }
+
+        /// <summary>
+        /// <para type="description">The ID of the repo to get branches from.</para>
+        /// </summary>
+        [Parameter(Mandatory = true)]
+        [ValidateNotNullOrEmpty()]
+        public string repositoryId { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            var creds = new VssBasicCredential(string.Empty, PAT);
+
+            // Connect to Azure DevOps Services
+            var connection = new VssConnection(new Uri(CollectionUri), creds);
+
+            // Get a GitHttpClient to talk to the Git endpoints
+            using (var gitClient = connection.GetClient<GitHttpClient>())
+            {
+                // Get data about a specific repository
+                var branches = gitClient.GetBranchesAsync(repositoryId).Result;
+
+                WriteObject(branches);
+            }
+        }
+    }
 }
